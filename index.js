c25e3608c78e71d9dc5cc7b0dbd0144f37e05106
Finn bear <finnbearlabs@gmail.com>
2020-01-30 15:08:57 -0800

    Multi-word state names.

--- index.js
+++ index.js
@@ -25,70 +25,66 @@ client.on('message', async message => {
 		} else if (command == "help") {
 			message.reply("Type `!query [state name or 2 letter code]`")
 		} else if (command.startsWith("query ") || command.startsWith("scan ")) {
-			if (segments.length != 2) {
-				message.reply("Expected 1 argument, the two-letter state code.");
+			const state = parser.state(segments.splice(1).join(" "));
+
+			if (!state) {
+				message.reply("Could not find state.");
+			} else if (!credentials.key && !(state in credentials.keys)) {
+				message.reply(`No LegiScan API key for state code ${state}.`);
 			} else {
-				const state = parser.state(segments.splice(1).join(" "));
+				channel.send(`Scanning for right-to-repair legislation in ${state}...`);
 
-				if (!state) {
-					message.reply("Could not find state.");
-				} else if (!credentials.key && !(state in credentials.keys)) {
-					message.reply(`No LegiScan API key for state code ${state}.`);
-				} else {
-					channel.send(`Scanning for right-to-repair legislation in ${state}...`);
+				const response = await api.search(state, query);
 
-					const response = await api.search(state, query);
+				// Debug
+				//console.log(response.searchresult);
 
-					// Debug
-					//console.log(response.searchresult);
-
-					if (response) {
-						let searchResult = "";
-						let bills = [];
-						for (let billIndex in response.searchresult) {
-							const bill = response.searchresult[billIndex];
-							if (!bill.text_url) {
-								continue;
-							}
-							const title = parser.title(bill);
-							if (parser.titleRelevance(title)) {
-								console.log(`Found bill "${title}"`);
-								bills.push(bill);
-							} else {
-								console.log(`Ignored bill "${title}"`);
-							}
+				if (response) {
+					let searchResult = "";
+					let bills = [];
+					for (let billIndex in response.searchresult) {
+						const bill = response.searchresult[billIndex];
+						if (!bill.text_url) {
+							continue;
 						}
-
-						// Most recently updated at the top
-						sortBills(bills);
-
-						if (bills.length > 0) {
-							const watchlist = await database.readWatchlist();
-
-							for (let bill of bills) {
-								if (bill.bill_id in watchlist) {
-									// TODO: Track changes
-									watchlist[bill.bill_id].last_action = bill.last_action;
-									watchlist[bill.bill_id].last_action_date = bill.last_action_date;
-								} else {
-									watchlist[bill.bill_id] = {title: bill.title, state: bill.state, status: new Date(bill.last_action_date) > new Date(2019, 0, 1) ? "new" : "expired", bill_number: bill.bill_number, last_action: bill.last_action, last_action_date: bill.last_action_date};
-								}
-								if (searchResult.length > 500) {
-									// Discord only supports 2000 max, so split into multiple messages
-									await channel.send(searchResult);
-									searchResult = "";
-								}
-								searchResult += `**${bill.bill_number}**: *${bill.title}* ${bill.last_action.toUpperCase()} as of \`${bill.last_action_date}\` (<${bill.text_url}>)\n`;
-							}
-
-							await database.updateWatchlist(watchlist);
+						const title = parser.title(bill);
+						if (parser.titleRelevance(title)) {
+							console.log(`Found bill "${title}"`);
+							bills.push(bill);
 						} else {
-							searchResult += `No current legislation found.`;
+							console.log(`Ignored bill "${title}"`);
 						}
-						await channel.send(searchResult);
-					} else {
-						message.reply("LegiScan API error.");
 					}
+
+					// Most recently updated at the top
+					sortBills(bills);
+
+					if (bills.length > 0) {
+						const watchlist = await database.readWatchlist();
+
+						for (let bill of bills) {
+							if (bill.bill_id in watchlist) {
+								// TODO: Track changes
+								watchlist[bill.bill_id].last_action = bill.last_action;
+								watchlist[bill.bill_id].last_action_date = bill.last_action_date;
+							} else {
+								watchlist[bill.bill_id] = {title: bill.title, state: bill.state, status: new Date(bill.last_action_date) > new Date(2019, 0, 1) ? "new" : "expired", bill_number: bill.bill_number, last_action: bill.last_action, last_action_date: bill.last_action_date};
+							}
+							if (searchResult.length > 500) {
+								// Discord only supports 2000 max, so split into multiple messages
+								await channel.send(searchResult);
+								searchResult = "";
+							}
+							searchResult += `**${bill.bill_number}**: *${bill.title}* ${bill.last_action.toUpperCase()} as of \`${bill.last_action_date}\` (<${bill.text_url}>)\n`;
+						}
+
+						await database.updateWatchlist(watchlist);
+					} else {
+						searchResult += `No current legislation found.`;
+					}
+					await channel.send(searchResult);
+				} else {
+					message.reply("LegiScan API error.");
 				}
 			}
 		}
