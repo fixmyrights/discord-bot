b5c624630573b9d7127d2fc79b409890b7a92b8f
Finn bear <finnbearlabs@gmail.com>
2020-01-30 15:04:46 -0800

    Tabs.

--- api.js
+++ api.js
@@ -4,21 +4,21 @@ const credentials = require("./credentials.json");
 const endpoint = "https://api.legiscan.com";
 
 exports.search = async function(state, query) {
-  const result = await axios.get(endpoint, {params: {
-    key: credentials.key || credentials.keys[state],
-    op: "search",
-    state,
-    query,
-    year: 1
-  }});
+	const result = await axios.get(endpoint, {params: {
+		key: credentials.key || credentials.keys[state],
+		op: "search",
+		state,
+		query,
+		year: 1
+	}});
 
-  const response = result.data;
+	const response = result.data;
 
-  if (response.status == "OK") {
-    return response;
-  } else {
-    console.log("API error:");
-    console.log(response);
-    return null;
-  }
+	if (response.status == "OK") {
+		return response;
+	} else {
+		console.log("API error:");
+		console.log(response);
+		return null;
+	}
 }
