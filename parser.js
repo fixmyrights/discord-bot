2306c0c09d48ca0616a682770069291881b1e52b
Finn bear <finnbearlabs@gmail.com>
2020-01-30 14:34:24 -0800

    Restore scan all option.

--- parser.js
+++ parser.js
@@ -3,6 +3,10 @@ const states = require("./states.json");
 exports.state = function(input) {
   const uppercaseInput = input.toUpperCase();
 
+  if (uppercaseInput == "ALL") {
+    return uppercaseInput;
+  }
+
   const stateByName = states.find(state => state.name == uppercaseInput);
   const stateByCode = states.find(state => state.code == uppercaseInput);
 
