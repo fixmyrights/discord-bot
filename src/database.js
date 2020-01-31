339d39f71c41a499f560b3c6da5222632891f5b9
Ashley Hutson <asheliahut@gmail.com>
2020-01-31 10:11:46 -0500

    PR - Add in linting and base docker image (#15)
    * Add in linting and base docker image
    
    * ran linter

--- src/database.js
+++ src/database.js
@@ -1,21 +1,16 @@
-const fs = require("fs");
-const util = require("util");
-
-const exists = util.promisify(fs.exists);
-const mkdir = util.promisify(fs.mkdir);
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
-const databaseDirectory = "./database/";
+const fs = require('fs').promises;
+const databaseDirectory = './database/';
 let database = {};
 let dirty = false;
 
 exports.load = async function() {
 	try {
-		database = JSON.parse(await readFile(`${databaseDirectory}database.json`));
+		database = JSON.parse(
+			await fs.readFile(`${databaseDirectory}database.json`, 'utf8'),
+		);
 		dirty = false;
 	} catch (err) {}
-}
+};
 
 exports.update = async function(bill) {
 	dirty = true;
@@ -26,20 +21,34 @@ exports.update = async function(bill) {
 
 	if (bill.bill_id in database.watchlist) {
 		if (bill.last_action != database.watchlist[bill.bill_id].last_action) {
-			console.log("Bill changed!");
+			console.log('Bill changed!');
 		}
 		database.watchlist[bill.bill_id].last_action = bill.last_action;
-		database.watchlist[bill.bill_id].last_action_date = bill.last_action_date;
+		database.watchlist[bill.bill_id].last_action_date =
+			bill.last_action_date;
 	} else {
-		database.watchlist[bill.bill_id] = {title: bill.title, state: bill.state, status: new Date(bill.last_action_date) > new Date(2019, 0, 1) ? "new" : "expired", bill_number: bill.bill_number, last_action: bill.last_action, last_action_date: bill.last_action_date};
+		database.watchlist[bill.bill_id] = {
+			title: bill.title,
+			state: bill.state,
+			status:
+				new Date(bill.last_action_date) > new Date(2019, 0, 1)
+					? 'new'
+					: 'expired',
+			bill_number: bill.bill_number,
+			last_action: bill.last_action,
+			last_action_date: bill.last_action_date,
+		};
 	}
-}
+};
 
 exports.save = async function(watchlist) {
-	if (!await exists(databaseDirectory)) {
-		await mkdir(databaseDirectory);
+	if (!(await fs.exists(databaseDirectory))) {
+		await fs.mkdir(databaseDirectory);
 	}
 
-	await writeFile(`${databaseDirectory}database.json`, JSON.stringify(database, null, "	"));
+	await fs.writeFile(
+		`${databaseDirectory}database.json`,
+		JSON.stringify(database, null, '	'),
+	);
 	dirty = false;
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const Discord = require("discord.js");
-const api = require("./api");
+// Discord
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("../data/config.json");
-const cron = require('node-cron');
-const database = require("./database");
-const credentials = require("../data/credentials.json");
-const parser = require("./parser");
+const credentials = require('../data/credentials.json');
 
+const discordClientKey =
+	process.env.DISCORD_CLIENT_KEY || credentials.client || null;
+const database = require('./database');
+
+const cron = require('node-cron');
+
+// Legiscan API
+const api = require('./api');
+const legiScanApiKey = process.env.LEGISCAN_API_KEY || credentials.key || null;
+
+// Utility
+const parser = require('./parser');
+const config = require('../data/config.json');
+const cronPostChannel = process.env.CHANNEL || config.channel || null;
 const debug = false;
 
-const query = `"right to repair" OR "right-to-repair" OR ((servicing OR repair) AND electronics) OR (fair AND electronic AND repair OR independent)`;
+const query =
+	'"right to repair" OR "right-to-repair" OR ((servicing OR repair) AND electronics) OR (fair AND electronic AND repair OR independent)';
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -37,72 +48,89 @@ const getBills = response => {
 	return bills;
 };
 
-const sortBills = bills => bills.sort((a, b) => new Date(b.last_action_date) - new Date(a.last_action_date));
+const sortBills = bills =>
+	bills.sort(
+		(a, b) => new Date(b.last_action_date) - new Date(a.last_action_date),
+	);
 
 client.on('message', async message => {
-	const channel = message.channel;
+	const { channel } = message;
 
-	if (channel.name && channel.name.includes("legi") && message.cleanContent.startsWith("!")) {
+	if (
+		channel.name &&
+		channel.name.includes('legi') &&
+		message.cleanContent.startsWith('!')
+	) {
 		const command = message.cleanContent.substring(1);
-		const segments = command.split(" ");
+		const segments = command.split(' ');
 
-		if (command == "ping") {
+		if (command === 'ping') {
 			message.reply('pong');
-		} else if (command == "help") {
-			message.reply("Type `!query [state name or 2 letter code]`")
-		} else if (command.startsWith("query ") || command.startsWith("scan ")) {
-			const state = parser.state(segments.splice(1).join(" "));
+		} else if (command === 'help') {
+			message.reply('Type `!query [state name or 2 letter code]`');
+		} else if (
+			command.startsWith('query ') ||
+			command.startsWith('scan ')
+		) {
+			const state = await parser.state(segments.splice(1).join(' '));
 
 			if (!state) {
-				message.reply("Could not find state.");
-			} else if (!credentials.key && !(state in credentials.keys)) {
+				message.reply('Could not find state.');
+			} else if (!legiScanApiKey && !(state in credentials.keys)) {
 				message.reply(`No LegiScan API key for state code ${state}.`);
 			} else {
-				channel.send(`Scanning for right-to-repair legislation in ${state}...`);
+				channel.send(
+					`Scanning for right-to-repair legislation in ${state}...`,
+				);
 
 				const response = await api.search(state, query);
+				let searchResult = '';
+				let bills = [];
 
 				// Debug
-				//console.log(response.searchresult);
+				// console.log(response.searchresult);
 
 				if (response) {
-					let searchResult = "";
-
-					const bills = getBills(response);
+					bills = getBills(response);
 
 					if (bills.length > 0) {
 						await database.load();
-
 						for (let bill of bills) {
 							database.update(bill);
 							if (searchResult.length > 500) {
 								// Discord only supports 2000 max, so split into multiple messages
 								await channel.send(searchResult);
-								searchResult = "";
+								searchResult = '';
 							}
-							searchResult += `**${bill.bill_number}**: *${parser.title(bill)}* ${bill.last_action.toUpperCase()} as of \`${bill.last_action_date}\` (<${bill.text_url}>)\n`;
+							searchResult += `**${
+								bill.bill_number
+							}**: *${parser.title(
+								bill,
+							)}* ${bill.last_action.toUpperCase()} as of \`${
+								bill.last_action_date
+							}\` (<${bill.text_url}>)\n`;
 						}
 
 						await database.save();
 					} else {
-						searchResult += `No current legislation found.`;
+						searchResult += 'No current legislation found.';
 					}
 					await channel.send(searchResult);
 				} else {
-					message.reply("LegiScan API error.");
+					message.reply('LegiScan API error.');
 				}
 			}
 		}
 	}
 });
 
-client.login(credentials.client);
+client.login(discordClientKey);
 
 cron.schedule('*/10 * * * *', async () => {
-	const response = await api.search("WA", query);
+	const response = await api.search('WA', query);
 
 	if (response) {
-		let searchResult = "";
+		let searchResult = '';
 
 		const bills = getBills(response);
 
@@ -113,7 +141,7 @@ cron.schedule('*/10 * * * *', async () => {
 				database.update(bill);
 			}
 
-			const channel = client.channels.find('name', config.channel)
+			const channel = client.channels.find('name', config.channel);
 			await channel.send(`Updated ${bills.length} automatically.`);
 
 			await database.save();
