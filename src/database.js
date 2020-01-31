86b117996258c944f95d14a550a79d231f83cd01
Finn bear <finnbearlabs@gmail.com>
2020-01-30 17:07:12 -0800

    Create database file if it doesn't exist.

--- src/database.js
+++ src/database.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 const util = require("util");
 
+const exists = util.promisify(fs.exists);
+const mkdir = util.promisify(fs.mkdir);
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-const databasePath = "./database/watchlist.json";
+const databaseDirectory = "./database/";
 
 exports.readWatchlist = async function() {
 	try {
-		return JSON.parse(await readFile(databasePath));
+		return JSON.parse(await readFile(`${databaseDirectory}watchlist.json`));
 	} catch (err) {
 		return {};
 	}
 }
 
 exports.updateWatchlist = async function(watchlist) {
-	await writeFile(databasePath, JSON.stringify(watchlist, null, "	"));
+	if (!await exists(databaseDirectory)) {
+		await mkdir(databaseDirectory);
+	}
+
+	await writeFile(`${databaseDirectory}watchlist.json`, JSON.stringify(watchlist, null, "	"));
 }
