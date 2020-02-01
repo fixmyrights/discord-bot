557339793fe3d31de8aeca339974f7bbfa8a5add
Finn Bear <finnbearlabs@gmail.com>
2020-01-31 20:14:13 -0800

    Channel config.

---
+++ src/commands/config/channel.js
@@ -0,0 +1,15 @@
+const database = require('../../database.js');
+
+exports.handle = async function(args, message, client) {
+  const value = args[0];
+
+  if (args.length == 0) {
+    message.reply(`Current channel name is #${database.getConfig('channel')}`);
+  } else if (args.length == 1) {
+    database.setConfig('channel', value.startsWith('#') ? value.substring(1) : value);
+    await database.save();
+    message.reply('Updated channel name');
+  } else {
+    message.reply('Invalid channel name.');
+  }
+};
--- src/commands/config/handler.js
+++ src/commands/config/handler.js
@@ -1,5 +1,6 @@
 const { logger } = require('./../../logger');
 
+const channel = require('./channel');
 const cron = require('./cron');
 
 exports.handle = function(args, message, client) {
@@ -7,12 +8,15 @@ exports.handle = function(args, message, client) {
   args = args.splice(1);
 
   switch (handler) {
+    case 'channel':
+      channel.handle(args, message, client);
+      break;
+
     case 'cron':
       cron.handle(args, message, client);
       break;
 
     case 'limit':
-    case 'channel':
       message.reply('not implemented');
       break;
 
