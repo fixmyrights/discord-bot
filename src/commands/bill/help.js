d4f726f9d833f46d287ce3e3945106d39628a9ab
Finn Bear <finnbearlabs@gmail.com>
2020-02-02 10:16:17 -0800

    Better description.

--- src/commands/bill/help.js
+++ src/commands/bill/help.js
@@ -1,8 +1,8 @@
 exports.handle = async function(args, message, client) {
   await message.reply('Here is a list of bill commands:');
   const { channel } = message;
-  await channel.send('Type `!bill scan [state]` to scan for new bills in a given state.');
+  await channel.send('Type `!bill scan [state name/code]` to scan for new bills in a given state.');
   await channel.send('Type `!bill add [bill id]` to add an unkown bill to the watchlist.');
-  await channel.send('Type `!bill watch/ignore [state] [bill #]` to add or remove a known bill from the watchlist.');
+  await channel.send('Type `!bill watch/ignore [state name/code] [bill #]` to add or remove a known bill from the watchlist.');
   await channel.send('Type `!bill watchlist` to see the entire watchlist.');
 };
--- src/commands/config/help.js
+++ src/commands/config/help.js
@@ -3,6 +3,6 @@ exports.handle = async function(args, message, client) {
   const { channel } = message;
   await channel.send('Type `!config channel [channel name]` to switch channels.');
   await channel.send('Type `!config cron [cron expression]` to set the schedule of automatic queries.');
-  await channel.send('Type `!config state [state]` to limit automatic queries to a single state.');
-  await channel.send('Type `!config interval [interval]` to configure time between detailed queries.');
+  await channel.send('Type `!config state [state name/code]` to limit automatic queries to a single state.');
+  await channel.send('Type `!config interval [minutes]` to configure time between detailed queries.');
 };
