20b5d0cffc6a4ea5b63ce0ad807a0ebcd2f8e11f
TheDevMinerTV <tobigames200@gmail.com>
2020-02-03 15:53:56 +0100

    🧐 Remove unneeded `async` and `await` statements

--- src/commands/bill/help.js
+++ src/commands/bill/help.js
@@ -1,8 +1,8 @@
-exports.handle = async function(_args, message, _client) {
+exports.handle = function(_args, message, _client) {
   let help = 'Here is a list of bill commands:\n';
   help += 'Type `!bill scan [state name/code]` to scan for new bills in a given state.\n';
   help += 'Type `!bill add [bill id]` to add an unkown bill to the watchlist.\n';
   help += 'Type `!bill watch/ignore [state name/code] [bill #]` to add or remove a known bill from the watchlist.\n';
   help += 'Type `!bill watchlist` to see the entire watchlist.';
-  await message.reply(help);
+  message.reply(help);
 };
--- src/commands/config/help.js
+++ src/commands/config/help.js
@@ -1,8 +1,8 @@
-exports.handle = async function(_args, message, _client) {
+exports.handle = function(_args, message, _client) {
   let help = 'Here is a list of configuration commands:\n';
   help += 'Type `!config channel [channel name]` to switch channels.\n';
   help += 'Type `!config cron [cron expression]` to set the schedule of automatic queries.\n';
   help += 'Type `!config state [state name/code]` to limit automatic queries to a single state.\n';
   help += 'Type `!config interval [minutes]` to configure time between detailed queries.';
-  await message.reply(help);
+  message.reply(help);
 };
