70cc938ca3a3d45f6d795eb18308d9b7aaf8d6c0
x47188 <48861389+x47188@users.noreply.github.com>
2020-02-03 21:54:07 -0500

    :sparkles: Start to implement a new legiscan service.

---
+++ src/services/legiscan.service.js
@@ -0,0 +1,52 @@
+const { stringify: querystring } = require('querystring');
+const axios = require('axios');
+const createError = require('axios/lib/core/createError');
+
+const { logger } = require('./../logger');
+
+const baseURL = process.env.LEGISCAN_ENDPOINT || 'https://api.legiscan.com';
+const timeout = process.env.LEGISCAN_TIMEOUT || 10 * 1000;
+
+exports.Legiscan = class Legiscan {
+  static get client() {
+    const client = axios.create({
+      timeout,
+      baseURL
+    });
+
+    client.interceptors.request.use(
+      config => {
+        config.params = config.params || {};
+
+        if (!config.params.key) {
+          config.params.key = process.env.LEGISCAN_API_KEY;
+        }
+
+        const { url, method, params } = config;
+
+        logger.debug(`>> ${method.toUpperCase()} ${baseURL} ${url} ${querystring({ ...params, key: 'redacted' })}`);
+
+        return config;
+      },
+      error => Promise.reject(error)
+    );
+
+    client.interceptors.response.use(
+      response => {
+        const { status, statusText, data, config, request } = response;
+
+        logger.debug(`<< ${status} ${statusText}`);
+        logger.trace(`<< ${JSON.stringify(data)}`);
+
+        if (data.status === 'ERROR') {
+          throw createError(`Legiscan: ${data.alert.message}`, config, response.statusText, request, response);
+        }
+
+        return response;
+      },
+      error => Promise.reject(error)
+    );
+
+    return client;
+  }
+};
