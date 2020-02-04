8d01000aac5a43a74c5d4d12d1afd7bc2f99c04e
x47188 <48861389+x47188@users.noreply.github.com>
2020-02-04 08:29:39 -0500

    :recycle: Refactor getBill legiscan service method.

--- src/services/legiscan.service.js
+++ src/services/legiscan.service.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const createError = require('axios/lib/core/createError');
 
 const { logger } = require('../logger');
-const { title: parseTitle, titleRelevance } = require('../parser');
+const { title: parseTitle, titleRelevance, timestamp } = require('../parser');
 
 const baseURL = process.env.LEGISCAN_ENDPOINT || 'https://api.legiscan.com';
 const timeout = process.env.LEGISCAN_TIMEOUT || 60 * 1000;
@@ -40,14 +40,7 @@ exports.Legiscan = class Legiscan {
         logger.trace(`<< ${JSON.stringify(data)}`);
 
         if (data.status === 'ERROR') {
-          // prettier-ignore
-          throw createError(
-            `Legiscan: ${data.alert.message}`,
-            config,
-            data.status,
-            request,
-            response
-          );
+          throw createError(`Legiscan: ${data.alert.message}`, config, data.status, request, response);
         }
 
         return response;
@@ -56,6 +49,39 @@ exports.Legiscan = class Legiscan {
     );
 
     return client;
+  }
+
+  static async getBill(id) {
+    try {
+      const { data } = Legiscan.client.get('/', {
+        op: 'getBill',
+        id
+      });
+      const { bill_id: billId, state, bill_number: number, title, history, calendar } = data.bill;
+
+      return {
+        state,
+        number,
+        title,
+        id: billId,
+        history: (history || []).map(item => ({ action: item.action, timestamp: new Date(item.date).valueOf() })),
+        calendar: (calendar || []).map(item => {
+          const { type, description, date: localDate, time: localTime, location } = item;
+
+          return {
+            type,
+            description,
+            localDate,
+            localTime,
+            location,
+            timestamp: timestamp(localDate, localTime, state)
+          };
+        })
+      };
+    } catch (err) {
+      logger.error(err);
+      return null;
+    }
   }
 
   static async search(state, query) {
@@ -81,15 +107,7 @@ exports.Legiscan = class Legiscan {
             return acc;
           }
 
-          // prettier-ignore
-          const {
-            bill_id: billId,
-            state,
-            bill_number: number,
-            title, url,
-            last_action: action,
-            last_action_date: timestamp
-          } = bill;
+          const { bill_id: billId, state, bill_number: number, title, url, last_action: action, last_action_date: timestamp } = bill;
 
           acc.push({
             state,
@@ -99,6 +117,7 @@ exports.Legiscan = class Legiscan {
             id: `${billId}`,
             history: [{ action, timestamp: new Date(timestamp).valueOf() }]
           });
+
           return acc;
         }, [])
         .sort((a, b) => b.history[0].timestamp - a.history[0].timestamp);
