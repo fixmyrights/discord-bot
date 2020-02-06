f46373b1e23326cee4fbb972b015f5d518631fb3
akuinu <39266030+akuinu@users.noreply.github.com>
2020-02-06 19:45:12 +0200

    Fix: Adding robustness to the bill and config handler's (#51)
    * Fix: Adding robustness to the bill and config handler's
    
    Now instead of crashing on incomplete commands bot will display help info instead
    
    #50
    
    * Refactoring to requests approach
    
    #50
    
    Co-authored-by: IHallik <IHallik@users.noreply.github.com>

--- src/commands/config/handler.js
+++ src/commands/config/handler.js
@@ -29,7 +29,7 @@ const canDoCommand = (command, message) => {
 };
 
 exports.handle = function(args, message, client) {
-  const handler = args.shift().toLowerCase();
+  const handler = (args.shift() || "").toLowerCase();
 
   if (!canDoCommand(handler, message)) {
     const notAllowedMsg = `You are not allowed to use the command \`${handler}\`.`;
