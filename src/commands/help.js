9926d94be06a904b00285e3afc0f879b770891d4
Simbaclaws <hylke.hellinga@gmail.com>
2020-02-07 01:00:58 +0100

    A simple help configuration change, where !help config and !help bill… (#52)
    * A simple help configuration change, where !help config and !help bill will be available, also !bill watchlist is now !bill list
    
    * forgot to add tripple equal sign
    
    * Fixed the issues @finnbear brought up in the comments

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,8 +1,17 @@
-exports.handle = function(_args, message, _client) {
-  let help = 'Here is a list of commands:\n';
-  help += 'Type `!ping`.\n';
-  help += 'Type `!help` for general help.\n';
-  help += 'Type `!bill help` for help regarding bills.\n';
-  help += 'Type `!config help` for help regarding configuration.';
-  message.reply(help);
+const billhelp = require('./bill/help');
+const confighelp = require('./config/help');
+
+exports.handle = function(args, message, _client) {
+  if (args[0] === 'bill') {
+    billhelp.handle(null, message, _client);
+  } else if (args[0] === 'config') {
+    confighelp.handle(null, message, _client);
+  } else {
+    let help = 'Here is a list of commands:\n';
+    help += 'Type `!ping`.\n';
+    help += 'Type `!help` for general help.\n';
+    help += 'Type `!bill help` for help regarding bills.\n';
+    help += 'Type `!config help` for help regarding configuration.';
+    message.reply(help);
+  }
 };
