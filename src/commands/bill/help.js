d9047998435f57e7d332fdf8f8babaf3858f5989
Finn Bear <finnbearlabs@gmail.com>
2020-02-07 22:52:33 -0800

    Help shows current prefix.

--- src/commands/bill/help.js
+++ src/commands/bill/help.js
@@ -1,9 +1,16 @@
+const database = require('../../database');
+
 exports.handle = function(_args, message, _client) {
+  let prefix = database.getConfig('prefix');
+  if (!prefix.endsWith('!')) {
+    prefix += ' ';
+  }
+
   let help = 'Here is a list of bill commands:\n';
-  help += 'Type `!bill scan [state name/code]` to scan for new bills in a given state.\n';
-  help += 'Type `!bill add [bill id]` to add an unkown bill to the watchlist.\n';
-  help += 'Type `!bill ignore [state name|state code] [bill #]` to remove a known bill from the watchlist.\n';
-  help += 'Type `!bill watch [state name|state code] [bill #]` to add a known bill to the watchlist.\n';
-  help += 'Type `!bill list` to see the entire watchlist.';
+  help += `Type \`${prefix}bill scan [state name/code]\` to scan for new bills in a given state.\n`;
+  help += `Type \`${prefix}bill add [bill id]\` to add an unkown bill to the watchlist.\n`;
+  help += `Type \`${prefix}bill ignore [state name|state code] [bill #]\` to remove a known bill from the watchlist.\n`;
+  help += `Type \`${prefix}bill watch [state name|state code] [bill #]\` to add a known bill to the watchlist.\n`;
+  help += `Type \`${prefix}bill list\` to see the entire watchlist.`;
   message.reply(help);
 };
--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,6 @@
 const billhelp = require('./bill/help');
 const confighelp = require('./config/help');
+const database = require('../database');
 
 exports.handle = function(args, message, client) {
   const arg = args.shift();
@@ -8,11 +9,15 @@ exports.handle = function(args, message, client) {
   } else if (arg === 'config') {
     confighelp.handle(args, message, client);
   } else {
+    let prefix = database.getConfig('prefix');
+    if (!prefix.endsWith('!')) {
+      prefix += ' ';
+    }
     let help = 'Here is a list of commands:\n';
-    help += 'Type `!ping`.\n';
-    help += 'Type `!help` for general help.\n';
-    help += 'Type `!bill help` for help regarding bills.\n';
-    help += 'Type `!config help` for help regarding configuration.';
+    help += `Type \`${prefix}ping\`.\n`;
+    help += `Type \`${prefix}help\` for general help.\n`;
+    help += `Type \`${prefix}bill help\` for help regarding bills.\n`;
+    help += `Type \`${prefix}config help\` for help regarding configuration.`;
     message.reply(help);
   }
 };
